1d0f6f0f7e74340948661e1680484e95c3abd48f
riadkabir45 <riadkabir45@gmail.com>
2025-08-07 14:36:18 +0600

    fix: unused var deployment issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import VarIntro from './pages/VarIntro'
 
 // <h1 className="text-3xl font-bold underline">Vite + React</h1>
